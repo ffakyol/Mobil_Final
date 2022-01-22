677fce7e405e9d5b1b435b900d60e48d5963ba87
ffakyol <59020078+ffakyol@users.noreply.github.com>
2022-01-22 07:58:22 +0300

    Add files via upload

---
+++ app/build/generated/source/buildConfig/debug/com/nexis/Feyza_Akyol_201813709028/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.nexis.Feyza_Akyol_201813709028;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.nexis.Feyza_Akyol_201813709028";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
